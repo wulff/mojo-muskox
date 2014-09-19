f15a3d7183671e40fa0e006b4816454c9e849ffc
Morten Wulff <wulff@ratatosk.net>
2014-09-19 16:17:30 +0200

    Improved Leaflet-related code, removed menu items

--- public/js/muskox.js
+++ public/js/muskox.js
@@ -1,25 +1,34 @@
 // Create the map
 var map = L.map('map');
 
-// // Create the basic tile layer using tiles from OpenStreetMap
-// var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// Create the basic tile layer using tiles from OpenStreetMap
+// var tiles = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //   attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
 //   minZoom: 6,
 //   maxZoom: 17,
 //   detectRetina: true
 // });
-// tileLayer.addTo(map);
+
+// Create a tile layer based on sat/map tiles from MapQuest
+var tiles = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
+  attribution: 'Tiles by <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  minZoom: 6,
+  maxZoom: 17,
+  detectRetina: true,
+  subdomains: '1234'
+});
 
 // Create a tile layer based on satellite imagery from Google
-var satellite = new L.Google('SATELLITE');
-map.addLayer(satellite);
+// var tiles = new L.Google('SATELLITE');
+
+map.addLayer(tiles).setView([74.4507, -20.550], 9);
 
 var geojsonTrackLayer = L.geoJson(null, {
   style: function (feature) {
     return {
-      color: '#f00',
-      opacity: 0.9,
-      weight: 3,
+      color: '#f60',
+      opacity: 0.8,
+      weight: 2,
     };
   },
 });
@@ -30,7 +39,7 @@ var geojsonPointLayer = L.geoJson(null, {
 
     content += '<strong>Ox: ' + feature.properties.animal_id + '</strong><br />';
     content += 'Altitude: ' + feature.properties.altitude + 'm<br />';
-    content += 'Temprature: ' + feature.properties.temperature + '˚C<br />';
+    content += 'Temperature: ' + feature.properties.temperature + '˚C<br />';
     content += 'Timestamp: ' + feature.properties.recorded + '<br />';
     content += 'Satellites: ' + feature.properties.sat_count + '<br />';
     content += 'Fix: ' + feature.properties.fix;
@@ -42,14 +51,12 @@ var geojsonPointLayer = L.geoJson(null, {
   }
 });
 
-$.getJSON('/v1/oxen/all/positions/points.json', function(data) {
+$.getJSON('/v1/oxen/all/positions/points.json?count=1', function(data) {
   geojsonPointLayer.addData(data);
   geojsonPointLayer.addTo(map);
 });
 
-$.getJSON('/v1/oxen/T5HS-4024/positions/lines.json', function(data) {
+$.getJSON('/v1/oxen/all/positions/lines.json?count=25', function(data) {
   geojsonTrackLayer.addData(data);
   geojsonTrackLayer.addTo(map);
 });
-
-map.setView([74.4107, -19.350], 14);
