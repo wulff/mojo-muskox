4ffc3fb6364cc993f1c74df02a7302326e7e2ea3
Morten Wulff <wulff@ratatosk.net>
2014-09-23 13:03:31 +0200

    Decreased minZoom setting

--- public/js/muskox.js
+++ public/js/muskox.js
@@ -4,7 +4,7 @@ var map = L.map('map');
 // Create a tile layer based on sat/map tiles from MapQuest
 var tiles = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
   attribution: 'Tiles by <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-  minZoom: 6,
+  minZoom: 4,
   maxZoom: 17,
   detectRetina: true,
   subdomains: '1234'
